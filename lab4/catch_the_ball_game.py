96b5d0aacd49fa2cbd1fd8de3c5eda2af8f3ea74
Islamov Sardor <isardor0707@gmail.com>
2021-10-08 16:06:49 +0300

    добавил функцию создания шариков и обработку события нажатия кнопки мыши

--- lab4/catch_the_ball_game.py
+++ lab4/catch_the_ball_game.py
@@ -15,13 +15,19 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+
 def new_ball():
-    '''рисует новый шарик '''
-    x = randint(100, 1100)
-    y = randint(100, 900)
-    r = randint(10, 100)
+    global x, y, r
+    x = randint(100,700)
+    y = randint(100,500)
+    r = randint(30,50)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
+
+def click(event):
+    print(x, y, r)
+    if abs(event.x - x) < r and abs(event.y - y) < r:
+        print('yeah!')
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -34,6 +40,7 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print('Click!')
+            click(event)
     new_ball()
     pygame.display.update()
     screen.fill(BLACK)
